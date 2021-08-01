a976c1bca610f66b3d7f9333dc0db7326500096b
coco <coco@example.com>
2021-08-01 20:33:07 +1200

    connect client-server

--- frontend/src/containers/Breeders.jsx
+++ frontend/src/containers/Breeders.jsx
@@ -1,5 +1,10 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+// apis
+import { fetchBreeders } from "../apis/breeders";
 
 export const Breeders = () => {
+  useEffect(() => {
+    fetchBreeders().then((data) => console.log(data));
+  }, []);
   return <Fragment>index Breeder</Fragment>;
 };
