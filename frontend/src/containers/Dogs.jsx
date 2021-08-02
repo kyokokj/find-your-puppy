685cb2251be04a8736d0e212565d315cf1083a60
coco <coco@example.com>
2021-08-02 12:34:38 +1200

    display dogs index

--- frontend/src/containers/Dogs.jsx
+++ frontend/src/containers/Dogs.jsx
@@ -1,16 +1,33 @@
-import React, { Fragment, useEffect } from "react";
-
+import React, { Fragment, useReducer, useEffect } from "react";
+// reducers
+import {
+  initialState as dogsInitialState,
+  dogsActionTypes,
+  dogsReducer,
+} from "../reducers/dogs";
 // apis
 import { fetchDogs } from "../apis/dogs";
+// constants
+import { REQUEST_STATE } from "../constants/constants";
 
 export const Dogs = ({ match }) => {
+  const [dogsState, dispatch] = useReducer(dogsReducer, dogsInitialState);
   useEffect(() => {
-    fetchDogs(match.params.breedersId).then((data) => console.log(data));
+    dispatch({ type: dogsActionTypes.FETCHING });
+    fetchDogs(match.params.breedersId).then((data) => {
+      dispatch({
+        type: dogsActionTypes.FETCH_SUCCESS,
+        payload: {
+          dogs: data.dogs,
+        },
+      });
+    });
   }, []);
   return (
     <Fragment>
-      index Dog
-      <p>Breeder id is {match.params.breedersId} </p>
+      {dogsState.dogsList.map((dog) => (
+        <div key={dog.id}>{dog.name}</div>
+      ))}
     </Fragment>
   );
 };
